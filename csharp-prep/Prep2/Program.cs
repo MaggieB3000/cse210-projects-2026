365f53cd56e8b4a048703e126b23947c2d84b029
MaggieB3000 <bin20017@byui.edu>
2026-01-10 22:27:53 -0700

    C# Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,55 @@
 using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string gradeLetter = "";
+        bool pass = true;
+        
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int gradePercentage = int.Parse(userInput);
+
+        if (gradePercentage >= 90)
+        {
+            gradeLetter = "A";
+        }
+
+        else if (gradePercentage >= 80)
+        {
+            gradeLetter = "B";
+        }
+
+        else if (gradePercentage >= 70)
+        {
+            gradeLetter = "C";
+        }
+
+        else if (gradePercentage >= 60)
+        {
+            gradeLetter = "D";
+            pass = false;
+        }
+
+        else
+        {
+            gradeLetter = "F";
+            pass = false;
+        }
+
+        Console.WriteLine($"Your letter grade is {gradeLetter}.");
+
+        if (pass == true)
+        {
+            Console.WriteLine("Congratulations! You passed!");
+        }
+
+        else
+        {
+            Console.WriteLine("You failed. Better luck next time.");
+        }
     }
 }
