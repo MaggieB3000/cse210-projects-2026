9799ab60336c41a479271c23f351cf7ce60a20ce
MaggieB3000 <bin20017@byui.edu>
2026-01-17 19:20:10 -0700

    Prep #3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int userGuess = -1;
+
+        Random randomMagicNumber = new Random();
+        int magicNumber = randomMagicNumber.Next(1,101);
+
+
+        //Console.Write("What is the magic number? ");
+        //string userInput = Console.ReadLine();
+        //magicNumber = int.Parse(userInput);
+
+        while (userGuess != magicNumber) 
+        {
+            Console.Write("What is your guess? ");
+            string userInput = Console.ReadLine();
+            userGuess = int.Parse(userInput);
+
+            if (userGuess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else if (userGuess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        } 
+
+
     }
 }
