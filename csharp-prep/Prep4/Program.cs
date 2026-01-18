f8de51a3e3c4e1b35e2fcc5d0546f8973705fc7c
MaggieB3000 <bin20017@byui.edu>
2026-01-17 19:58:37 -0700

    Prep #4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
+        int userNumber = -1;
+        int listSum = 0;
+        int listMax = 0;
+
         Console.WriteLine("Hello Prep4 World!");
+
+        List<int> numberList = new List<int>();
+
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a list of numbers and type \'0\' when you\'re finished. ");
+            string userInput = Console.ReadLine();
+            userNumber = int.Parse(userInput);
+
+            if (userNumber != 0)
+            {
+                numberList.Add(userNumber);
+            }
+        }
+
+        //#1 & #3
+        foreach (int number in numberList)
+        {
+            listSum = listSum + number;
+
+            if (listMax < number)
+            {
+                listMax = number;
+            }
+        }
+
+        //#2
+        float listAverage = listSum / numberList.Count;
+
+        Console.WriteLine($"The sum is: {listSum}");
+        Console.WriteLine($"The average is: {listAverage}");
+        Console.WriteLine($"The max/largest number is: {listMax}");
+
     }
 }
