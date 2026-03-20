f23d8d4f57cac48c583d8c9f2751aef97198ba69
MaggieB3000 <bin20017@byui.edu>
2026-03-20 14:27:09 -0600

    Learning 05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapesList = new List<Shape>();
+
+        Square Square = new Square(20, "red");
+        shapesList.Add(Square);
+        Rectangle Rectangle = new Rectangle(20, 30, "blue");
+        shapesList.Add(Rectangle);
+        Circle Circle = new Circle(10, "yellow");
+        shapesList.Add(Circle);
+
+        foreach (Shape shape in shapesList)
+        {
+            Console.WriteLine($"{shape.GetColor()} - {shape.GetArea()}");
+        }
     }
 }
