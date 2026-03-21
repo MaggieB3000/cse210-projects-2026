a01580389b057dcffeef849072319286d34b529e
MaggieB3000 <bin20017@byui.edu>
2026-03-20 20:49:46 -0600

    Learning 03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        int counter = 20;
+        Random random = new Random();
+
+        while (counter > 0)
+        {
+            Fraction fraction1 = new Fraction();
+            fraction1.SetTop(random.Next(1, 100));
+            fraction1.SetBottom(random.Next(1, 100));
+            Fraction fraction2 = new Fraction(random.Next(1, 100));
+            Fraction fraction3 = new Fraction(random.Next(1, 100), random.Next(1, 100));
+
+            Console.WriteLine($"{fraction1.GetFractionString()}");
+            Console.WriteLine($"{fraction1.GetDecimalValue()}");
+
+            Console.WriteLine($"{fraction2.GetFractionString()}");
+            Console.WriteLine($"{fraction2.GetDecimalValue()}");
+
+            Console.WriteLine($"{fraction3.GetFractionString()}");
+            Console.WriteLine($"{fraction3.GetDecimalValue()}");
+
+            counter = counter - 1;
+        }
+
     }
 }
